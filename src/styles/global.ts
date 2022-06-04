8edbb079401c0f826bca897ab9707ee603a13e2f
Emanoel Barros <ebarros297@gmail.com>
2022-06-04 11:18:06 -0300

    feat: add Roboto font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,11 +2,11 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-      sans-serif;
     margin: 0;
     box-sizing: border-box;
+    
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
     color: #333333;
   }
 

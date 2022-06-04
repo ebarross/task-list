701cf8e91b2b6ab9b4d195c845f2bff831b7fd0e
Emanoel Barros <ebarros297@gmail.com>
2022-06-04 10:10:47 -0300

    chore: add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-unused-vars': 'warn',
+    'import/extensions': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: [
+          '.tsx',
+        ],
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+        ],
+      },
+    },
+  },
+};

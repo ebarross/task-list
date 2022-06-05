d034c38e76016f506aeac6755c05b1ff8e2a72ae
Emanoel Barros <ebarros297@gmail.com>
2022-06-05 06:45:16 -0300

    refactor: change data types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,20 @@
-export type Section = {
-  id?: number;
-  title: string;
-  color: string;
+export type Task = {
+  id: number;
+  text: string;
 };
 
-export type Task = {
-  id?: number;
-  sectionId: number;
+export type TaskData = {
   text: string;
+};
+
+export type Section = {
+  id: number;
+  title: string;
+  color: string;
+  tasks: Task[];
+};
+
+export type SectionData = {
+  title: string;
+  color: string;
 };
